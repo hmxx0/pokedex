093252f36271469134e9ff8b4c07e9e3b5ccd429
Hirtik Maru <hirtikmaru@hotmail.com>
2023-11-16 14:24:18 +0000

    edited the navigation bar

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -5,7 +5,12 @@ export default function GuestLayout() {
   return (
     <>
       <header className='p-3 flex justify-between items-center bg-black'>
-        <span className="text-lg font-semibold text-pink-600">Logo</span>
+        <span className="text-lg font-semibold text-pink-600">Search
+          <img src="" alt="" />
+        </span>
+        <span className="text-4xl font-semibold text-pink-600">Pokédex
+          <img src="" alt="" />
+        </span>
         <nav className="flex items-center gap-5">
           <Link to="/">Home</Link>
           
--- src/pages/SimpleSlider/Home.jsx
+++ src/pages/SimpleSlider/Home.jsx
@@ -7,32 +7,32 @@ export default function SimpleSlider () {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1
       };
     return (
         <div className="md:container md:mx-auto">
             <Slider {...settings}>
-                <div className="flex items-center justify-center">
-                    <img className="mx-auto w-1/3 object-cover" src="/Balls/1.png" alt=""/>
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
+                    <img className="mx-auto w-1/3 object-cover " src="/Balls/1.png" alt=""/>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="mx-auto w-1/3 object-cover" src="/Balls/2.png" alt=""/>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="mx-auto w-1/3 object-cover" src="/Balls/3.png" alt=""/>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="mx-auto w-1/3 object-cover" src="/Balls/4.png" alt=""/>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="mx-auto w-1/3 object-cover" src="/Balls/5.png" alt=""/>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="mx-auto w-1/3 object-cover" src="/Balls/6.png" alt=""/>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="mx-auto w-1/3 object-cover" src="/Balls/7.png" alt=""/>
                 </div>
                 <div className="flex items-center justify-center">
