ab72d3bd21aa407650a54234719109a42775792a
Hirtik Maru <hirtikmaru@hotmail.com>
2023-12-07 18:10:53 +0000

    more css

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -4,14 +4,14 @@ import { Link, Outlet } from "react-router-dom"
 export default function GuestLayout() {
   return (
     <>
-      <header className='p-3 bg-black'>
-        <div className='absolute text-4xl left-1/2 font-font-mono'>Pokédex</div>
-        <nav className="flex p-3 text-xl">
-          <div className='mr-auto'>
+      <header className='p-6 bg-red-500'>
+        <div className='absolute text-center right-96 left-96 text-8xl top-0  font-font-mono animate-pulse transition-shadow text-stone-700 font-bold'>Pokédex</div>
+        <nav className="flex p-2 text-xl">
+          <div className='mr-auto mr-4 transition ease-in-out delay-150 bg-zinc-800 hover:-translate-y-1 hover:scale-110 hover:bg-red-700 duration-300 rounded-md p-2'>
             <Link to="/">Home</Link>
           </div>
           <div>
-            <Link className='mr-4' to="/all">View All</Link>
+            <Link className='mr-4 transition ease-in-out delay-150 bg-zinc-800 hover:-translate-y-1 hover:scale-110 hover:bg-red-700 duration-300 rounded-md p-2' to="/all">View All</Link>
           </div>
         </nav>
       </header>
--- src/pages/SimpleSlider/Home.jsx
+++ src/pages/SimpleSlider/Home.jsx
@@ -20,49 +20,49 @@ export default function SimpleSlider () {
                 <div className="hover:animate-bounce w-6 h-10">
                     <img className="relative top-20 mx-auto w-5/12 object-cover " src="/Balls/1.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Pokémon is based around building a small team of monsters ,<br />to battle other monsters in a quest to become the best.</p>
+                        <p className="text-3xl text-emerald-600 font-bold">Pokémon is based around building a small team of monsters ,<br />to battle other monsters in a quest to become the best.</p>
                     </div>
                 </div>
                 <div className="hover:animate-bounce w-6 h-10 ">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/2.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Pokémon are divided into types, such as water and fire,<br /> each with different strengths and weaknesses.  </p>
+                        <p className="text-3xl text-emerald-600 font-bold">Pokémon are divided into types, such as water and fire,<br /> each with different strengths and weaknesses.  </p>
                     </div>
                 </div>
                 <div className="hover:animate-bounce w-6 h-10">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/3.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Each Pokémon belongs to a specific species and has its own set of abilities, characteristics, and stats.</p>
+                        <p className="text-3xl text-emerald-600 font-bold">Each Pokémon belongs to a specific species and has its own set of abilities, characteristics, and stats.</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/4.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Trainers use devices called Poké Balls to capture and store Pokémon. <br />When a wild Pokémon is weakened in battle, a Poké Ball can be thrown to attempt to catch it.</p>
+                        <p className="text-3xl text-emerald-600 font-bold">Trainers use devices called Poké Balls to capture and store Pokémon. <br />When a wild Pokémon is weakened in battle, a Poké Ball can be thrown to attempt to catch it.</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/5.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Legendary Pokémon are rare, powerful Pokémon that are closely related to local lore surrounding the creation or destruction of a certain region. There is only one of each in the world.</p>
+                        <p className="text-3xl text-emerald-600 font-bold">Legendary Pokémon are rare, powerful Pokémon that are closely related to local lore surrounding the creation or destruction of a certain region. There is only one of each in the world.</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/6.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Another is that Lugia is actually an extremely dangerous Pokemon - one flap from its wings can cause a 40-day storm. In order to keep others safe, it spends most of its time gliding silently at the bottom of the ocean.</p>
+                        <p className="text-3xl text-emerald-600 font-bold">Another is that Lugia is actually an extremely dangerous Pokemon - one flap from its wings can cause a 40-day storm. In order to keep others safe, it spends most of its time gliding silently at the bottom of the ocean.</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-center hover:animate-bounce w-6 h-10">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/7.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Moltres is a powerful Pokemon who can usually avoid severe injury, but if it ever does find itself in need of healing, it has a spectacular method of doing so. It dives into a volcano and uses the lava to deal with its injuries.</p>
+                        <p className="text-3xl text-emerald-600 font-bold">Moltres is a powerful Pokemon who can usually avoid severe injury, but if it ever does find itself in need of healing, it has a spectacular method of doing so. It dives into a volcano and uses the lava to deal with its injuries.</p>
                     </div>
                 </div>
                 <div className="flex items-center justify-center">
                     <img className="relative top-20 mx-auto w-5/12 object-cover" src="/Balls/8.png" alt="" />
                     <div className="text-center">
-                        <p className="text-3xl">Xerneas is an immortal Pokemon that has lived for thousands of years. It sometimes sleeps in the form of a tree. But when it's awake, it can use its branch-like antlers to bestow immortality to others.</p>
+                        <p className="text-3x l text-emerald-600 font-bold">Xerneas is an immortal Pokemon that has lived for thousands of years. It sometimes sleeps in the form of a tree. But when it's awake, it can use its branch-like antlers to bestow immortality to others.</p>
                     </div>
                 </div>
             </Slider>
